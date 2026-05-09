51c7a31b6076c1087c07b8cfda2a287461bda32f
siddharthpsp <269622639+siddharthpsp@users.noreply.github.com>
2026-05-09 05:15:03 -0400

    Support GitHub Pages subpath route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,8 @@ function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/bg-payment-tracker"} component={Home} />
+      <Route path={"/bg-payment-tracker/"} component={Home} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
